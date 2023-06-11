d77f56f1bd86f16b62ff3c1e76c7ba12e5f512a1
unknown <pakequis@gmail.com>
2023-06-11 12:49:27 -0300

    first commit

---
+++ Firmware/src/main.cpp
@@ -0,0 +1,361 @@
+/*
+  Bad Thing of the Edge Mechanical Keyboard Firmware
+  by Rodrigo Feliciano
+  https://www.github.com/pakequis
+  https://www.youtube.com/pakequis
+*/
+
+#include <Arduino.h>
+#include <EEPROM.h>
+/*
+  Adafruit_TinyUSB library and example from:
+  https://github.com/adafruit/Adafruit_TinyUSB_Arduino
+*/
+#include "Adafruit_TinyUSB.h"
+
+#define LED1  0   /* Left LED   */
+#define LED2  28  /* Center LED */
+#define LED3  16  /* Top LED    */
+#define LED4  11  /* Right LED  */
+
+/* LED Effects */
+#define LED_BREATH    0 
+#define LED_KEYPRESS  1
+#define LED_NOISE     2
+#define LED_QUAKE     3
+#define LED_BREATH2   4
+#define LED_NONE      5
+
+#define KEYNUMBER 20  /* Number of keys */
+#define ACTIVESTATE false
+
+/* Key status for keys (core 0) to LED effects (core 1) */
+#define NOKEY         0
+#define KEYPRESS      1
+#define CHANGE_EFFECT 2
+
+uint8_t const effects[] = {LED_NONE, LED_BREATH, LED_BREATH2, LED_NOISE, LED_QUAKE, LED_KEYPRESS};
+uint8_t effects_size = sizeof(effects) / sizeof(effects[0]);  
+
+/* Effects arrays */
+
+/* LED breath effect */
+uint8_t const breath_array[] = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120,
+                                  130, 140, 155, 165, 175, 175, 165, 155, 140, 130, 
+                                  120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0};
+
+uint8_t breath_size = sizeof(breath_array) / sizeof(breath_array[0]);                       
+
+/* Quake flicker light effect */
+/* Original quake flicker style from https://github.com/id-Software/Quake/blob/master/qw-qc/world.qc 
+   Values from the style 10 and tick of 100 ms: 
+   // 10 FLUORESCENT FLICKER
+	 lightstyle(10, "mmamammmmammamamaaamammma");
+*/
+uint8_t const quake_array[] = {255, 255, 0, 255, 0, 255, 255, 255, 255, 0, 255, 255, 
+                         0, 255, 0, 255, 0, 0, 0, 255, 0, 255, 255, 255, 0};
+
+uint8_t quake_size = sizeof(quake_array) / sizeof(quake_array[0]);              
+
+/* Key press effect */
+uint8_t const keypress_array[] = { 0, 10, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180,
+                                  200, 220, 240, 255, 255, 240, 220, 200, 180, 160, 
+                                  140, 120, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0};
+
+uint8_t keypress_size = (uint8_t) (sizeof(keypress_array) / sizeof(keypress_array[0]));   
+
+// HID report descriptor using TinyUSB's template
+// Single Report (no ID) descriptor
+uint8_t const desc_hid_report[] =
+{
+  TUD_HID_REPORT_DESC_KEYBOARD()
+};
+
+// USB HID object.
+// desc report, desc len, protocol, interval, use out endpoint
+Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD, 2, false);
+
+/*
+  Key Map:
+  [0] [1] [2] [3] [4]
+  [5] [6] [7] [8] [9]
+    [a]   [b]
+                 [c]    
+    [d] [e] [f]
+                 [g]
+    [h]          [i]
+                 [j]
+
+  0 SW5   GPIO4   | a SW13  GPIO26
+  1 SW2   GPIO1   | b SW15  GPIO21
+  2 SW1   GPIO7   | c SW10  GPIO13
+  3 SW17  GPIO19  | d SW12  GPIO27
+  4 SW19  GPIO17  | e SW14  GPIO22
+  5 SW6   GPIO5   | f SW16  GPIO20
+  6 SW3   GPIO2   | g SW9   GPIO12
+  7 SW4   GPIO3   | h SW11  GPIO6
+  8 SW18  GPIO18  | i SW8   GPIO10
+  9 SW20  GPIO14  | j SW7   GPIO9
+*/
+
+/* Key pins */
+int key_pins[] = { D4, D1, D7, D19, D17, D5, D2, D3, D18, D14, 
+                  D26, D21, D13, D27, D22, D20, D12, D6, D10, D9 };
+
+/* keycodes for Diablo III Game */
+uint8_t hidcode[] = { HID_KEY_NONE, HID_KEY_PRINT_SCREEN, HID_KEY_Z, HID_KEY_O, HID_KEY_Y,
+                      HID_KEY_F, HID_KEY_S, HID_KEY_G, HID_KEY_ENTER, HID_KEY_ALT_LEFT, 
+                      HID_KEY_ESCAPE, HID_KEY_I, HID_KEY_M, HID_KEY_SHIFT_LEFT, HID_KEY_2, 
+                      HID_KEY_3, HID_KEY_4, HID_KEY_1, HID_KEY_SPACE, HID_KEY_Q
+                    };
+
+int led_count = 0;
+int led_count2 = breath_size / 2;
+
+bool fifo_flag = false;
+bool effect_change_flag = false;
+
+uint32_t core1_var = NOKEY;
+
+byte EEPROM_value = 0;
+
+/********************************
+  Core 0 - USB Keyboard 
+ ********************************/
+void setup() 
+{
+  TinyUSB_Device_Init(0);
+  usb_hid.setBootProtocol(HID_ITF_PROTOCOL_KEYBOARD);
+  usb_hid.setPollInterval(2);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+  usb_hid.setStringDescriptor("TinyUSB Keyboard");
+
+  usb_hid.begin();
+
+  // Set up pin as input
+  for (byte i=0; i< KEYNUMBER; i++)
+  {
+    pinMode(key_pins[i], INPUT_PULLUP);
+  }
+
+  delay(100);
+  
+  // wait until device mounted
+  while( !TinyUSBDevice.mounted() ) delay(1);
+};
+
+void loop() 
+{
+  // poll gpio once each 2 ms
+  delay(2);
+
+  // used to avoid send multiple consecutive zero report for keyboard
+  static bool keyPressedPreviously = false;
+
+  uint8_t count = 0;
+  uint8_t keycode[6] = { 0 };
+
+  /* Check LED effect key */
+  if (ACTIVESTATE == digitalRead(key_pins[0]))
+  {
+    while(ACTIVESTATE == digitalRead(key_pins[0]))
+    {
+      delay(10); /* debounce */
+    }
+
+    fifo_flag = rp2040.fifo.push_nb(CHANGE_EFFECT);
+    delay(100);
+  }
+
+  //scan normal key and send report
+  for(uint8_t i = 1; i < KEYNUMBER; i++)
+  {
+    if ( ACTIVESTATE == digitalRead(key_pins[i]) )
+    {
+      // if pin is active (low), add its hid code to key report
+      keycode[count++] = hidcode[i];
+
+      // 6 is max keycode per report
+      if (count == 6) break;
+    }
+  }
+
+  if ( TinyUSBDevice.suspended() && count )
+  {
+    // Wake up host if we are in suspend mode
+    // and REMOTE_WAKEUP feature is enabled by host
+    TinyUSBDevice.remoteWakeup();
+  }
+
+  // skip if hid is not ready e.g still transferring previous report
+  if ( !usb_hid.ready() ) return;
+
+  if ( count )
+  {
+    // Send report if there is key pressed
+    uint8_t const report_id = 0;
+    uint8_t const modifier = 0;
+
+    /* Inform Core 1 */
+    fifo_flag = rp2040.fifo.push_nb(KEYPRESS);
+
+    keyPressedPreviously = true;
+    usb_hid.keyboardReport(report_id, modifier, keycode);
+  } 
+  else
+  {
+    // Send All-zero report to indicate there is no keys pressed
+    // Most of the time, it is, though we don't need to send zero report
+    // every loop(), only a key is pressed in previous loop()
+    if ( keyPressedPreviously )
+    {
+      keyPressedPreviously = false;
+      usb_hid.keyboardRelease(0);
+    }
+  }
+}
+
+/***************************************
+  Core 1 - LED control
+****************************************/
+void setup1() 
+{
+  /* EEPROM for save the current LED effect */
+  EEPROM.begin(256);
+
+  /* All LED pins as outputs */
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+
+  /* All LED off */
+  analogWrite(LED1, 0);
+  analogWrite(LED2, 0);
+  analogWrite(LED3, 0);
+  analogWrite(LED4, 0);
+
+  delay(250);
+
+  /* Get the LED effect from EEPROM */
+  EEPROM_value = EEPROM.read(0);
+}
+
+void loop1() 
+{
+  /* Check if Core 0 send anything */
+  fifo_flag = rp2040.fifo.pop_nb(&core1_var);
+
+  /* LED Effect change */
+  if(core1_var == CHANGE_EFFECT)
+  {
+        EEPROM_value++;
+        if (EEPROM_value >= effects_size)
+        {
+          EEPROM_value = 0;
+        }
+
+        /* Save the new effect in EEPROM */
+        EEPROM.write(0, EEPROM_value);
+        EEPROM.commit();
+
+        /* Blink the center LED to inform the change */
+        analogWrite(LED2, 255);
+        delay(100);
+        analogWrite(LED2, 0);
+        delay(100);
+
+        /* Clean the FIFO */
+        while(rp2040.fifo.available()) 
+        {
+          fifo_flag = rp2040.fifo.pop_nb(&core1_var);
+        }
+        core1_var = NOKEY;
+        delay(100);
+  }
+
+  /* Check the current LED effect */
+  switch (effects[EEPROM_value])
+  {
+    /* All LEDs up and down bright */
+    case LED_BREATH:
+      analogWrite(LED1, breath_array[led_count]);
+      analogWrite(LED2, breath_array[led_count]);
+      analogWrite(LED3, breath_array[led_count]);
+      analogWrite(LED4, breath_array[led_count]);
+      led_count++;
+      if(led_count >= breath_size) led_count = 0;
+      delay(100);
+      break;
+  
+    /* Random noise in all LEDs */
+    case LED_NOISE:
+      led_count = (int)random(127) + 100;
+      analogWrite(LED1, led_count);
+      analogWrite(LED2, led_count);
+      analogWrite(LED3, led_count);
+      analogWrite(LED4, led_count);
+      delay(random(100));
+      break;
+
+    /* LEDs Up and Down, first the center */
+    case LED_BREATH2:
+      analogWrite(LED1, breath_array[led_count2]);
+      analogWrite(LED2, breath_array[led_count]);
+      analogWrite(LED3, breath_array[led_count2]);
+      analogWrite(LED4, breath_array[led_count2]);
+      led_count++;
+      led_count2++;
+      if(led_count >= breath_size) led_count = 0;
+      if(led_count2 >= breath_size) led_count2 = 0;
+      delay(100);
+      break;
+
+    /* Effect activate on keypress */
+    case LED_KEYPRESS:
+      //fifo_flag = rp2040.fifo.pop_nb(&core1_var);
+      if(core1_var == KEYPRESS)
+      {
+        for (led_count = 0; led_count < keypress_size; led_count++)
+        {
+          analogWrite(LED1, keypress_array[led_count]);
+          analogWrite(LED2, keypress_array[led_count]);
+          analogWrite(LED3, keypress_array[led_count]);
+          analogWrite(LED4, keypress_array[led_count]);
+          delay(10);
+        }
+
+        /* Clean the FIFO */
+        while(rp2040.fifo.available()) 
+        {
+          fifo_flag = rp2040.fifo.pop_nb(&core1_var);
+        }
+        core1_var = NOKEY;
+        delay(100);
+        led_count = 0;
+      }
+      break;
+
+    /* Classical Quake flicker light */
+    case LED_QUAKE:
+      int value;
+      value = quake_array[led_count];
+      analogWrite(LED1, value);
+      analogWrite(LED2, value);
+      analogWrite(LED3, value);
+      analogWrite(LED4, value);
+
+      delay(100);
+      led_count++;
+      if (led_count >= quake_size) led_count = 0;
+      break;
+
+    /* No Effect */
+    default:
+      analogWrite(LED1, 0);
+      analogWrite(LED2, 0);
+      analogWrite(LED3, 0);
+      analogWrite(LED4, 0);
+      break;
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# Bad-Thing-of-the-Edge-keyboard
-Mechanical keyboard for left hand with Raspberry Pi Pico for games.
+# **Bad Thing of the Edge keyboard**
+A mechanical gamer keyboard for left hand with Raspberry Pi Pico.
+
+![Keyboard prototype](Images/keyboard-prototype.jpg)
+
+## Why another mechanical keyboard?
+
+Sometimes I play **Diablo 3** on my small 7-inch laptop and have difficulty with the keyboard. The keys are very small and the placement is a little different from a regular keyboard. Yes, I could remap the keys or use a regular keyboard, but it wouldn't be as fun as making my own keyboard.
+
+![Prototypes](Images/prototypes.jpg)
+
+The keyboard layout was made for my left hand with the main four fingers on the skills buttons and the thumb on the potion button.
+
+It should work for Diablo 4 (the action keys are mostly in the same positions), but I haven't tested it yet (**$$$**). For other games, you need to change the buttons table in the firmware.
+
+## Demo video:
+[![Demo Video](https://youtu.be/LNERu-DqztY/default.jpg)](https://youtu.be/LNERu-DqztY)
+
+## Hardware:
+
+The boards were developed in kicad 7.0. There are two PCBs, one for the keyboard (Main-board) and another for the panel. The two boards are stacked and secured with M3 screws and 8 mm spacers.
+
+![Boards](Images/boards.jpg)
+
+External libraries used in the project::
+[Key Switchs library for Kicad by Rafael Silva.](https://github.com/kiswitch/kiswitch)
+[Raspberry Pi Pico by Nicolò Carandini.](https://github.com/ncarandini/KiCad-RP-Pico)
+
+I used two-piece clear keycaps for the keys ([bought on Aliexpress](https://s.click.aliexpress.com/e/_DEuJarX) - **Affiliate link!**). I printed the button images, cut them out and put them inside the keycaps.
+
+Bars for the LEDs were made into the panel board with keep out areas on the top and bottom copper layers and the bottom solder mask layer. When the LEDs on the main board turn on, the light is scattered on these bars. I used red LEDs to contrast with the black solder mask and match the horror theme of the keyboard.
+
+The stack:
+![Boards](Images/prototype-leds.jpg)
+
+Mechanical keys are Cherry MX type, but in the prototype I took the keys from a old PTZ camera controller.
+
+Files here are from the second version of the boards, with corrections made to the prototype. The photos and videos are from the prototype. One problem I had with the prototype panel board was that I didn't put the keep out area on the bottom solder mask layer. So I had to sand these areas and the effect was even better than I expected. Here the affected area:
+
+![LED bar área](Images/prototype-panel-correction.jpg)
+
+## Firmware:
+
+The firmware was made in [VSCode](https://code.visualstudio.com/) with the [PlatformIO](https://platformio.org/) extension and the [Arduino-Pico core](https://github.com/earlephilhower/arduino-pico), using [Adafruit's TinyUSB library](https://github.com/adafruit/Adafruit_TinyUSB_Arduino). Core 0 is responsible for the keyboard and Core 1 for the LED effects. It was a bit over the top, but I wanted to test this functionality.
+
+![Board Prototype](Images/prototype-board.jpg)
+The keyboard part was made using the [example from Adafruit's TinyUSB library](https://github.com/adafruit/Adafruit_TinyUSB_Arduino/blob/master/examples/HID/hid_boot_keyboard/hid_boot_keyboard.ino).
+
+The LED effects in this first version are:
+- **Breath** - Up and down the bright of the LED bars.
+- **Noise** - Random change in the bright of the bars.
+- **Breath 2** - Same as the first one, but the center bar changes the brightness first.
+- **Keypress** - Flashes the bars when a key is pressed.
+- **Quake Flicker** - The classical [fluorescent Quake's flashing lights](https://github.com/Pakequis/arduino-quake-flicker-lamp).
+
+One of the keys - the first one on the top - was exclusive to change the effect of the LEDs. When it is pressed, the bars flash indicating the change and the new effect starts working. Effects are saved in one byte of the FLASH memory and retrieved on power on.
+
+## Bad Thing of What?
+The name comes from a famous Brazilian case of haunted house and exorcism that happened in 1953. On one of the exorcism nights, Father Pedro Cintra asked in Latin: **"Praecipio ibi ut dicas qui sis tu"** ("I command you to tell me who you are"). A monstrous voice then replied in Portuguese: **"Eu sou o Diabo!"** ("I am the Devil!").
+
+But people didn't pronounce that name and called the entity just **"Coisa Ruim"** (Bad Thing) when the rumors spread. They added part of the name of the city where the case took place, **Borda da Mata** (something like "Edge of the Woods"), and started to call it Coisa Ruim da Borda (Bad Thing of the Edge).
+
+
+I found an [article in a magazine of the time with a picture of the haunted house](http://memoria.bn.br/DocReader/DocReader.aspx?bib=003581&pagfis=86382) (in Portuguese - page 102).
+
+## Known issues:
+- The position of the Raspberry pi pico on the board is not good and some USB cables may not fit between the boards.
+- It lacks a case for 3D printing. 
+- The positioning of the LEDs could be improved.
+
+## Contact:
+
+Feel free to reach out to me on social media: @pakequis in any of them...
+
+You can also send me an email at pakequis (Gmail).
+
+
+
